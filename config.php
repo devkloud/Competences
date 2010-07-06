e3d847110e55cfab833f749fe6e81affbae7a360
Meroje <meroje.f@gmail.com>
2010-07-06 15:07:26 +0200

    Correction d'un probleme de securité et ajout de commentaires

--- config.php
+++ config.php
@@ -19,7 +19,7 @@ if (!isset($index)) {
 	$config['local'] = array('host'=>'localhost', 
 		'port'=>3306, 
 		'user'=>'web', 
-		'pass'=>'kloudweb', 
+		'pass'=>'****', 
 		'db'=>'fbb_home'
 		);
 	/*
