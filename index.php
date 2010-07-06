81b828a482e3aba4493c4cb577f0b96daaaee972
Meroje <meroje.f@gmail.com>
2010-07-06 15:07:26 +0200

    Correction d'un probleme de securité et ajout de commentaires

--- index.php
+++ index.php
@@ -15,40 +15,43 @@ require_once ('data.php');
     <head>
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <title>Compétences</title>
+<!-- Plugin Star rating pour un meilleur rendu visuel -->
         <script src="http://jquery.com/src/jquery-latest.js">
         </script>
         <script src='http://jquery-star-rating-plugin.googlecode.com/svn/trunk/jquery.rating.js' type="text/javascript" language="javascript">
         </script>
         <link href='http://jquery-star-rating-plugin.googlecode.com/svn/trunk/jquery.rating.css' type="text/css" rel="stylesheet"/>
+<!-- End Star rating -->
     </head>
     <body>
         <div id="container">
             <?php
+            // Début de la boucle de listing des catégories
             for ($nbr = 0; $nbr <= $nbr_des; $nbr++) {
-                if ($categorie[$nbr]) {
-                	if ($nbr !=0) {
-                		echo '</fieldset>';
+                if ($categorie[$nbr]) { // Si la catégorie existe
+                	if ($nbr !=0) { // si ce n'est pas la première affichée ...
+                		echo '</fieldset>'; // on ferme le fieldset précédemment ouvert
 					}
-                    echo '<h1>'.$categorie[$nbr]['nom'].'</h1>';
-                    foreach ($valeur as $v) {
-                        if ($designation[$v['designation_id']]['categories_id'] == $nbr) {
-                            if ($designation[$v['designation_id']]['designation'] != $prec) {
-                                echo '</fieldset><fieldset title="'.$designation[$v['designation_id']]['designation'].'"><legend>'.$designation[$v['designation_id']]['designation'].'</legend>';
+                    echo '<h1>'.$categorie[$nbr]['nom'].'</h1>'; // Titre de la catégorie
+                    foreach ($valeur as $v) { // Listing des notes
+                        if ($designation[$v['designation_id']]['categories_id'] == $nbr) { // Si la catégorie contenant la désignation est la bonne
+                            if ($designation[$v['designation_id']]['designation'] != $prec) { // La désignation n'est plus la même
+                                echo '</fieldset><fieldset title="'.$designation[$v['designation_id']]['designation'].'"><legend>'.$designation[$v['designation_id']]['designation'].'</legend>'; // Donc on commence un nouveau fieldset
                             }
-                            echo '<label>'.$user[$v['users_id']]['username'];
-                            for ($note = 1; $note < 6; $note++) {
-                                if ($note == $v['note']) {
+                            echo '<label>'.$user[$v['users_id']]['username']; // Affichge du nom de l'utilisateur correspondant à la note et la désignation
+                            for ($note = 1; $note < 6; $note++) { // Boucle d'affichage de la note
+                                if ($note == $v['note']) { // Si on a atteint la note donnée on met checked
                                     echo '<input name="'.$user[$v['users_id']]['username'].'.'.$designation[$v['designation_id']]['designation'].'" type="radio" value="'.$note.'" class="star" disabled="disabled" checked="checked" />';
-                                } else {
+                                } else { // sinon on le met pas
                                     echo '<input name="'.$user[$v['users_id']]['username'].'.'.$designation[$v['designation_id']]['designation'].'" type="radio" value="'.$note.'" class="star" disabled="disabled" />';
                                 }
                             }
-                            if ($v['commentaire'] != NULL) {
+                            if ($v['commentaire'] != NULL) { // Si un commentaire est défini on l'affiche
                                 echo ' ('.$v['commentaire'].')';
                             }
-                            echo '</label><br />';
+                            echo '</label><br />'; // On referme le label de l'utilisateur
                         }
-                        $prec = $designation[$v['designation_id']]['designation'];
+                        $prec = $designation[$v['designation_id']]['designation']; // on définit la vraible prec pour savoir quand on aura changé
                     }
                 }
             }
