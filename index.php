2b9009c41fe5c2218de733253d8f8987d3333aa1
Jérôme Foray <meroje.f@gmail.com>
2010-07-11 12:39:06 +0200

    Ajout de DataTables.js, JqueryUI ThemeRoller et un peu de css.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 ﻿<?php
 // Redéfinition des variables
 $donnees = array();
-$cat_indent	 = 0;
+$cat_indent = 0;
 
 // Récupération et traitement des données
 $index = true;
@@ -12,10 +12,22 @@ require_once ('data.php');
 <head>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 <title>Compétences</title>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="http://datatables.net/release-datatables/media/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.2.custom.min.js"></script>
+<link rel="stylesheet" href="css/hot-sneaks/jquery-ui-1.8.2.custom.css" type="text/css" media="screen" />
+<style type="text/css" media="screen">
+table {
+	width: 100%;
+}
+tr.odd {
+	background-color: #E2E4FF;
+}
+</style>
 </head>
 <body>
 <div id="container">
-<table>
+<table id="competences">
 	<thead>
 		<tr>
 			<th>ID</th>
@@ -36,9 +48,20 @@ require_once ('data.php');
 			<td><?= $i->note ?></td>
 			<td><?= $i->commentaire ?></td>
 		</tr>
-		<? endforeach; ?>
+	<? endforeach; ?>
 	</tbody>
 </table>
 </div>
+<script type="text/javascript">
+	$(document).ready( function() {
+		$('#competences').dataTable({
+			"bJQueryUI": true,
+			"sPaginationType": "full_numbers",
+			"oLanguage": {
+				"sUrl": "js/french.txt"
+			}
+		});
+	});
+</script>
 </body>
 </html>
